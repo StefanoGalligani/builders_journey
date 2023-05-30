30fc89b741b12425ae4f22d08ac6c614948a9b7b
StefanoGalligani <stefanogalligani0987@gmail.com>
2023-05-30 22:24:53 +0200

    Fixed bug in WheelPieceController, changed joint break forces, added two levels
    Amend: level 3

--- Assets/Pieces/Scripts/WheelPieceController.cs
+++ Assets/Pieces/Scripts/WheelPieceController.cs
@@ -17,6 +17,8 @@ namespace BuilderGame.Pieces {
 
         internal override void UpdatePiece()
         {
+            if (_joint) return;
+            
             _motorSpeed = 0;
             if (Input.GetKey(KeyCode.D)) {
                 _motorSpeed += _speed;
