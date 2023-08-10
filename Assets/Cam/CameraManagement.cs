3576d3ea3e297b426a00595e3074683cb46fde2f
StefanoGalligani <stefanogalligani0987@gmail.com>
2023-08-10 15:51:33 +0200

    Added single prefab to contain all level prefabs, camera movement, changed prices

--- Assets/Cam/CameraManagement.cs
+++ Assets/Cam/CameraManagement.cs
@@ -8,16 +8,31 @@ namespace BuilderGame.Cam
     public class CameraManagement : MonoBehaviour
     {
         [SerializeField] private CinemachineVirtualCamera _cinemachine;
+        [SerializeField] private Vector3 _originalOffset;
+        private CinemachineFramingTransposer _transposer;
         private Transform _vehicleTransform;
+        private bool _buildingPhase = true;
 
         void Start()
         {
             _vehicleTransform = GameObject.FindObjectOfType<Vehicle>().transform;
-            _cinemachine.Follow = _vehicleTransform;
+            _transposer = _cinemachine.GetComponentInChildren<CinemachineFramingTransposer>();
+            _transposer.m_TrackedObjectOffset = _originalOffset;
             FindObjectOfType<StartNotifier>().GameStart += OnGameStart;
         }
 
+        void Update() {
+            if (!_buildingPhase) return;
+            if (Input.GetMouseButton(2)) {
+                Vector3 mouseDelta = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+                _transposer.m_TrackedObjectOffset -= mouseDelta * Time.deltaTime * 70;
+            }
+        }
+
         private void OnGameStart() {
+            _buildingPhase = false;
+            _transposer.m_TrackedObjectOffset = _originalOffset;
+
             _cinemachine.Follow = _vehicleTransform.GetChild(0);
         }
     }
