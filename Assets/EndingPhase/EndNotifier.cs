86c45e4e2a737d1662035d7268443a3a3c31b5f2
StefanoGalligani <stefanogalligani0987@gmail.com>
2023-08-19 15:38:38 +0200

    Fixed cannon sprite, added star sprite, minor changes

--- Assets/EndingPhase/EndNotifier.cs
+++ Assets/EndingPhase/EndNotifier.cs
@@ -6,12 +6,14 @@ namespace BuilderGame.EndingPhase
     public class EndNotifier : MonoBehaviour
     {
         public event Action GameEnd;
+        [SerializeField] SpriteRenderer spriteRenderer;
         private bool _activated = false;
         private void OnTriggerEnter2D(Collider2D other) {
             if (_activated || !other.gameObject.layer.Equals(LayerMask.NameToLayer("Vehicle"))) return;
 
             _activated = true;
             GameEnd?.Invoke();
+            Destroy(spriteRenderer);
         }
     }
 }
